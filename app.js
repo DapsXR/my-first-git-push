dc7526625f12efe1c52e5960790eef99c4528adb
Dapo <dapo@rethinkreality.io>
2022-03-24 12:32:42 +0000

    added function changeHeadercolor and eventlistner to header element with changeHeaderColor function.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('Hello')
+
+function changeHeaderColor(event){
+        color= window.prompt ("What color do you want the text to be")
+        event.target.style.color=color
+    }
+
+const headerColor=document.querySelector('h1');
+// headerColor.addEventlistener('click', function () {
+//     console.log("title has been clicked");
+// })
+headerColor.addEventListener('click', changeHeaderColor )
